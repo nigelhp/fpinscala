c26ecbfb4a00d1ede40ae1ab704a7681fe033b7e
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-03-02 09:23:35 +0000

    Ex 2.1 - Use 1-based n for Fibonacci

--- src/main/scala/Exercise2_1.scala
+++ src/main/scala/Exercise2_1.scala
@@ -10,8 +10,8 @@
 object Exercise2_1 {
 
   def fib(n: Int): Int = {
-    if (n == 0) 0
-    else if (n == 1) 1
+    if (n == 1) 0
+    else if (n == 2) 1
     else fib(n-1) + fib(n-2)
   }
 }
--- src/test/scala/Exercise2_1Spec.scala
+++ src/test/scala/Exercise2_1Spec.scala
@@ -3,27 +3,27 @@ import org.scalatest.FlatSpec
 
 class Exercise2_1Spec extends FlatSpec {
 
-  "Fibonacci(0)" should "be 0" in {
-    assert(fib(0) === 0)
-  }
-
-  "Fibonacci(1)" should "be 1" in {
-    assert(fib(1) === 1)
+  "Fibonacci(1)" should "be 0" in {
+    assert(fib(1) === 0)
   }
 
   "Fibonacci(2)" should "be 1" in {
     assert(fib(2) === 1)
   }
 
-  "Fibonacci(3)" should "be 2" in {
-    assert(fib(3) === 2)
+  "Fibonacci(3)" should "be 1" in {
+    assert(fib(3) === 1)
   }
 
-  "Fibonacci(4)" should "be 3" in {
-    assert(fib(4) === 3)
+  "Fibonacci(4)" should "be 2" in {
+    assert(fib(4) === 2)
   }
 
-  "Fibonacci(5)" should "be 5" in {
-    assert(fib(5) === 5)
+  "Fibonacci(5)" should "be 3" in {
+    assert(fib(5) === 3)
+  }
+
+  "Fibonacci(6)" should "be 5" in {
+    assert(fib(6) === 5)
   }
 }
