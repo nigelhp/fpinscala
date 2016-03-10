4acffe69720d3c9f20b7651467544f6462b6eda9
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-03-10 08:59:27 +0000

    Ex 3.3 - setHead

---
+++ src/main/scala/Exercise3_3.scala
@@ -0,0 +1,14 @@
+import fpinscala.datastructures._
+
+/*
+ * Exercise 3.3
+ * Using the same idea, implement the function setHead for replacing the first
+ * element of a List with a different value.
+ */
+object Exercise3_3 {
+
+  def setHead[A](head: A, list: List[A]): List[A] = list match {
+    case Nil => Cons(head, Nil)
+    case Cons(_, t) => Cons(head, t)  
+  }
+}
---
+++ src/test/scala/Exercise3_3Spec.scala
@@ -0,0 +1,16 @@
+import org.scalatest.FlatSpec
+
+import fpinscala.datastructures._
+import Exercise3_3.setHead
+
+class Exercise3_3Spec extends FlatSpec {
+
+  "setHead" should "change the head of an existing list" in {
+    assert(setHead(9, List(1, 2, 3)) === List(9, 2, 3))
+    assert(setHead(9, List(1)) === List(9))
+  }
+
+  it should "change the head of an empty list" in {
+    assert(setHead(9, Nil) === List(9))
+  }
+}
