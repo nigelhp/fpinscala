f3b9507636f9af74dd93e5f3fac1d3cd9570de6b
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-03-14 18:05:22 +0000

    Ex 3.4 - use book's List type rather than the Scala built-in

--- src/main/scala/Exercise3_4.scala
+++ src/main/scala/Exercise3_4.scala
@@ -1,5 +1,7 @@
 import scala.annotation.tailrec
 
+import fpinscala.datastructures._
+
 /*
  * Exercise 3.4
  * Generalize tail to the function drop, which removes the first n elements from a list.
@@ -11,5 +13,10 @@ import scala.annotation.tailrec
 object Exercise3_4 {
 
   @tailrec
-  def drop[A](l: List[A], n: Int): List[A] = if (n == 0) l else drop(l.tail, n-1)
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n == 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
+    }
 }
--- src/test/scala/Exercise3_4Spec.scala
+++ src/test/scala/Exercise3_4Spec.scala
@@ -1,37 +1,35 @@
-import Exercise3_4.drop
 import org.scalatest.FlatSpec
+
+import fpinscala.datastructures._
+import Exercise3_4.drop
 
 class Exercise3_4Spec extends FlatSpec {
 
-  "drop 0" should "leave a non-empty list intact" in {
+  "drop 0" should "return an unchanged list when the list is not empty" in {
     assert(drop(List(1, 2, 3), 0) === List(1, 2, 3))
   }
 
-  "drop 1" should "remove the head from a non-empty list" in {
+  "drop 1" should "remove the head when the list is not empty" in {
     assert(drop(List(1, 2, 3), 1) === List(2, 3))
   }
 
-  "drop 2" should "retain only the last element from a three element list" in {
+  "drop 2" should "retain only the last element when the list has three elements" in {
     assert(drop(List(1, 2, 3), 2) === List(3))
   }
 
-  "drop n" should "remove all elements from a list of size n" in {
+  "drop n" should "remove all elements when the list has size n" in {
     assert(drop(List(1, 2, 3), 3) === Nil)
   }
 
-  "drop n+1" should "throw an exception when the list has size n" in {
-    intercept [UnsupportedOperationException] {
-      drop(List(1, 2, 3), 4)
-    }
+  "drop n+1" should "remove all elements when the list has size n" in {
+    assert(drop(List(1, 2, 3), 3) === Nil)
   }
 
-  "drop 0" should "leave an empty list intact" in {
+  "drop 0" should "return the empty list when the list is empty" in {
     assert(drop(Nil, 0) == Nil)
   }
 
-  "drop 1" should "throw an exception when the list is empty" in {
-    intercept [UnsupportedOperationException] {
-      drop(Nil, 1)
-    }
+  "drop 1" should "return the empty list when the list is empty" in {
+    assert(drop(Nil, 1) === Nil)
   }
 }
