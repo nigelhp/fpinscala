3fa9b52c3bd4723de6d6901e6457b8e1a3a8ce2e
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-03-19 20:39:29 +0000

    Ex 3.6 - init

---
+++ src/main/scala/Exercise3_6.scala
@@ -0,0 +1,11 @@
+/*
+ * Exercise 3.6
+ * Not everything works out so nicely.  Implement a function, init, that returns a List consisting of
+ * all but the last element of a List.  So, given List(1, 2, 3, 4), init will return List(1, 2, 3).
+ * Why can't this function be implemented in constant time like tail?
+ *
+ * def init[A](l: List[A]): List[A]
+ */
+class Exercise3_6 {
+
+}
