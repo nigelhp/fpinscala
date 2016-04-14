696aadb0ccd0e7284eb7bd2257661e5f4eb0ec84
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-04-14 12:19:07 +0100

    Ex 3.9 - removed unnecessary braces

--- src/main/scala/Exercise3_9.scala
+++ src/main/scala/Exercise3_9.scala
@@ -8,7 +8,5 @@ import Exercise3_7.foldRight
  */
 object Exercise3_9 {
 
-  def length[A](list: List[A]):Int = {
-    foldRight(list, 0)((_, count) => count + 1)
-  }
+  def length[A](list: List[A]):Int = foldRight(list, 0)((_, count) => count + 1)
 }
