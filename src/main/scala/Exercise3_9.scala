02b627fd8db441317878ca2be723e66194642895
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-04-14 12:18:13 +0100

    Ex 3.9 - list length in terms of foldRight

--- src/main/scala/Exercise3_9.scala
+++ src/main/scala/Exercise3_9.scala
@@ -1,3 +1,6 @@
+import fpinscala.datastructures._
+import Exercise3_7.foldRight
+
 /*
  * Exercise 3.9
  * Compute the length of a list using foldRight.
@@ -5,4 +8,7 @@
  */
 object Exercise3_9 {
 
+  def length[A](list: List[A]):Int = {
+    foldRight(list, 0)((_, count) => count + 1)
+  }
 }
---
+++ src/test/scala/Exercise3_9Spec.scala
@@ -0,0 +1,19 @@
+import Exercise3_9.length
+import org.scalatest.FlatSpec
+
+import fpinscala.datastructures._
+
+class Exercise3_9Spec extends FlatSpec {
+
+  "length" should "return 0 when the list is empty" in {
+    assert(length(Nil) == 0)
+  }
+
+  it should "return 1 when the list contains a single element" in {
+    assert(length(List(42)) == 1)
+  }
+  
+  it should "return n when the list contains n elements" in {
+    assert(length(List(1, 2, 3)) == 3)
+  }
+}
