fc93404844d50c4ac4a1b13de1230e53effc9053
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-04-14 12:20:09 +0100

    Ex 3.11 - sum, product, length via foldLeft

---
+++ src/main/scala/Exercise3_11.scala
@@ -0,0 +1,15 @@
+import fpinscala.datastructures._
+import Exercise3_10.foldLeft
+
+/*
+ * Exercise 3.11
+ * Write sum, product, and a function to compute the length of a list using foldLeft.
+ */
+object Exercise3_11 {
+
+  def sum(list: List[Int]): Int = foldLeft(list, 0)((acc, e) => acc + e)
+
+  def product(list: List[Int]): Int = foldLeft(list, 1)((acc, e) => acc * e)
+
+  def length[A](list: List[A]): Int = foldLeft(list, 0)((acc, _) => acc + 1)
+}
