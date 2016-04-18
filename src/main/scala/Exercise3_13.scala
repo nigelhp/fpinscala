ea8dbf580bb621def7347fb697e23798282fb4b9
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-04-18 17:52:57 +0100

    Ex 3.13 - foldLeft & foldRight implemented by the other

--- src/main/scala/Exercise3_13.scala
+++ src/main/scala/Exercise3_13.scala
@@ -1,3 +1,6 @@
+import Exercise3_12.reverse
+import fpinscala.datastructures._
+
 /*
  * Exercise 3.13
  * Can you write foldLeft in terms of foldRight?
@@ -5,9 +8,13 @@
  * Implementing foldRight via foldLeft is useful because it lets us implement foldRight tail-recursively, which means
  * it works even for large lists without overflowing the stack.
  */
-class Exercise3_13 {
+object Exercise3_13 {
+
+  def foldLeft[A, B](list: List[A], acc:B)(f: (B, A) => B): B = {
+    Exercise3_7.foldRight(reverse(list), acc)((e, acc) => f(acc, e))
+  }
 
   def foldRight[A, B](list: List[A], acc: B)(f: (A, B) => B): B = {
-    
+    Exercise3_10.foldLeft(reverse(list), acc)((acc, e) => f(e, acc))
   }
 }
