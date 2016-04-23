9074b25077e65d4a54817b7d0390f131460a7fd0
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-04-23 22:17:11 +0100

    Ex 3.16 - increment all integers in a list by one

---
+++ src/main/scala/Exercise3_16.scala
@@ -0,0 +1,14 @@
+import Exercise3_7.foldRight
+import fpinscala.datastructures._
+
+/*
+ * Exercise 3.16
+ * Write a function that transforms a list of integers by adding 1 to each element.
+ * (Reminder: this should be a pure function that returns a new List!)
+ */
+object Exercise3_16 {
+
+  def increment(list: List[Int]): List[Int] = {
+    foldRight(list, Nil: List[Int])((i, acc) => Cons(i + 1, acc))
+  }
+}
