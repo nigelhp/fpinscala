68bbe5e42f9870c0c58e07b08b91a7a73507ded0
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-04-25 22:10:45 +0100

    Ex 3.22 - zipAdd

---
+++ src/main/scala/Exercise3_22.scala
@@ -0,0 +1,26 @@
+import fpinscala.datastructures._
+
+/*
+ * Exercise 3.22
+ * Write a function that accepts two lists and constructs a new list by adding corresponding elements.
+ * For example, List(1, 2, 3) and List(4, 5, 6) become List(5, 7, 9).
+ */
+object Exercise3_22 {
+
+//  def zipAdd(xs: List[Int], ys: List[Int]): List[Int] = {
+//    xs match {
+//      case Nil => Nil
+//      case Cons(xh, xt) => ys match {
+//        case Nil => Nil
+//        case Cons(yh, yt) => Cons(xh + yh, zipAdd(xt, yt))
+//      }
+//    }
+//  }
+  
+  // introducing a pair, and pattern-matching on that results in a cleaner approach than nested pattern matches (above)
+  def zipAdd(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(xh, xt), Cons(yh, yt)) => Cons(xh + yh, zipAdd(xt, yt))
+  }
+}
