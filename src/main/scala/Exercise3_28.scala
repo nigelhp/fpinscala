6c1cc7e8dd6a778511143a982968e5683a5fdd35
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-05-10 18:28:44 +0100

    Ex 3.28 - tree map

--- src/main/scala/Exercise3_28.scala
+++ src/main/scala/Exercise3_28.scala
@@ -1,8 +1,14 @@
+import fpinscala.datastructures._
+
 /*
  * Exercise 3.28
- * Write a function map, analogous to the method of the same name on List, that modifies each element in a tree with a
- * given function.
+ * Write a function map, analogous to the method of the same name on List, that 
+ * modifies each element in a tree with a given function.
  */
 object Exercise3_28 {
 
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 }
