5cea22e0966069da8fa0bbb4f8c8981d21f29362
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-05-13 13:02:03 +0100

    Ex 3.25 - tree size

--- src/main/scala/Exercise3_25.scala
+++ src/main/scala/Exercise3_25.scala
@@ -1,7 +1,14 @@
+import fpinscala.datastructures._
+
 /*
  * Exercise 3.25
- * Write a function size that counts the number of nodes (leaves and branches) in a tree.
+ * Write a function size that counts the number of nodes (leaves and branches) 
+ * in a tree.
  */
 object Exercise3_25 {
 
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => size(l) + size(r) + 1
+  }
 }
