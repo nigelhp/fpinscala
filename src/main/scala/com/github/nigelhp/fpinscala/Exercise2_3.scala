3e9e5afe8326ab36ed646a9d259e9765e6b37674
Nigel Perkins <nigelhp@yahoo.co.uk>
2016-11-28 22:14:43 +0000

    Formatting

--- src/main/scala/com/github/nigelhp/fpinscala/Exercise2_3.scala
+++ src/main/scala/com/github/nigelhp/fpinscala/Exercise2_3.scala
@@ -6,16 +6,16 @@ package com.github.nigelhp.fpinscala
  * into a function of one argument that partially applies f.  Here again there's only one
  * implementation that compiles.  Write this implementation:
  *
- * def curry[A,B,C](f: (A,B) => C): A => (B => C)
+ * def curry[A,B,C](f: (A, B) => C): A => (B => C)
  */
 object Exercise2_3 {
 
   /*
    * Takes a value and a function of two arguments, and returns a function of one argument.
    */
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+  def partial1[A,B,C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
-  def curry[A,B,C](f: (A,B) => C): A => (B => C) =
-    (a: A) => (b: B) => f(a, b)
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
 }
