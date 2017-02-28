cb58a28904b64c9e84f08579a2163a8fb6ad0a43
Nigel Perkins <nigelhp@yahoo.co.uk>
2017-02-28 15:45:36 +0000

    Reduce dependency on pattern-matching

--- src/main/scala/com/github/nigelhp/fpinscala/Exercise4_1.scala
+++ src/main/scala/com/github/nigelhp/fpinscala/Exercise4_1.scala
@@ -27,31 +27,48 @@ package com.github.nigelhp.fpinscala.exercise4_1
  */
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = this match {
-    case None => None
-    case Some(value) => Some(f(value))
-  }
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case None => None
+      case Some(value) => Some(f(value))
+    }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = this match {
-    case None => None
-    case Some(value) => f(value)
-  }
+  // using pattern-matching
+  // def flatMap[B](f: A => Option[B]): Option[B] =
+  //   this match {
+  //     case None => None
+  //     case Some(value) => f(value)
+  //   }
 
-  def getOrElse[B >: A](default: => B): B = this match {
-    case None => default
-    case Some(value) => value
-  }
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
-    case None => ob
-    case Some(_) => this
-  }
+  def getOrElse[B >: A](default: => B): B =
+    this match {
+      case None => default
+      case Some(value) => value
+    }
 
-  def filter(f: A => Boolean): Option[A] = this match {
-    case None => None
-    case Some(value) => if (f(value)) this else None
-  }
+  // using pattern-matching
+  // def orElse[B >: A](ob: => Option[B]): Option[B] =
+  //   this match {
+  //     case None => ob
+  //     case Some(_) => this
+  //   }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
+
+  // using pattern-matching
+  // def filter(f: A => Boolean): Option[A] =
+  //   this match {
+  //     case Some(value) if f(value) => this
+  //     case _ => None
+  //   }
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
 }
 
-case class Some[+A](value: A) extends Option[A]
 case object None extends Option[Nothing]
+case class Some[+A](value: A) extends Option[A]
