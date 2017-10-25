44ac5338f1b0ebb6cfef194f07453c2cb3b59631
Nigel Perkins <nigelhp@yahoo.co.uk>
2017-10-25 13:44:40 +0100

    Ex 9.1: Parser map2 & many1

---
+++ src/main/scala/com/github/nigelhp/fpinscala/Exercise9_1.scala
@@ -0,0 +1,21 @@
+package com.github.nigelhp.fpinscala
+
+import fpinscala.parsers.Parsers
+
+import scala.language.higherKinds
+
+/*
+ * Exercise 9.1
+ * Using product, implement the now familiar combinator map2 and then use this to implement many1 in
+ * terms of many.
+ *   def map2[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C]
+ */
+trait Exercise9_1[ParseError, Parser[+_]] extends Parsers[ParseError, Parser] {
+  def map2[A, B, C](p: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] = {
+    val parser = product(p, p2)
+    map(parser)(f.tupled)
+  }
+
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p)) { (a, manyA) => a :: manyA }
+}
---
+++ src/main/scala/fpinscala/parsers/Parsers.scala
@@ -0,0 +1,18 @@
+package fpinscala.parsers
+
+import scala.language.higherKinds
+
+trait Parsers[ParseError, Parser[+_]] { self =>
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  def char(c: Char): Parser[Char]
+  def string(s: String): Parser[String]
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def many[A](p: Parser[A]): Parser[List[A]]
+  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+
+  def slice[A](p: Parser[A]): Parser[String]
+  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A, B)]
+}
