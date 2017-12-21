d7949d33a2821b9f0c998105533ecd01367ab485
Nigel Perkins <nigelhp@yahoo.co.uk>
2017-12-21 18:10:48 +0000

    Ex 11.3: sequence & traverse

---
+++ src/main/scala/com/github/nigelhp/fpinscala/Exercise11_3.scala
@@ -0,0 +1,25 @@
+package com.github.nigelhp.fpinscala
+
+import fpinscala.monads.Monad
+
+import scala.language.higherKinds
+
+/*
+ * Exercise 11.3
+ * The sequence and traverse combinators can be implemented once and for all on Monad[F].
+ *   def sequence[A](lma: List[F[A]]): F[List[A]]
+ *   def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]]
+ */
+object Exercise11_3 {
+  trait Exercise11_3Monad[F[_]] extends Monad[F] {
+    def sequence[A](lfa: List[F[A]]): F[List[A]] =
+      lfa.foldRight(unit(List.empty[A])) { (fa, fas) =>
+        map2(fa, fas)(_ :: _)
+      }
+
+    def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
+      la.foldRight(unit(List.empty[B])) { (a, fbs) =>
+        map2(f(a), fbs)(_ :: _)
+      }
+  }
+}
